ec104069dc5bd88e1aea830100bb34fe96f87cff
Tom Pinnock <tomj.pinnock@outlook.com>
2019-07-05 22:54:46 +0100

    More API match

--- include/blt.h
+++ include/blt.h
@@ -17,13 +17,14 @@ typedef struct dimensions_t_ {
 #	if defined(BLT_BUILDING_LIB)
 #		define BLT_API __declspec(dllexport)
 #   else
-#      defined BLT_API __declspec(dllimport)
+#      define BLT_API __declspec(dllimport)
 #   endif
 #else
 #   if defined(BLT_BUILDING_LIB) && __GNUC__ >= 4
 #		define BLT_API __attribute__ ((visibility ("default")))  
 #   else
 #       defined BLT_API
+#	endif
 #endif
 
 #ifdef __cplusplus
@@ -58,7 +59,22 @@ color_t terminal_pick_color(int x, int y, int index);
 color_t terminal_pick_bkcolor(int x, int y);
 
 void terminal_put_ext(int x, int y, int dx, int dy, int code, color_t* corners);
+dimensions_t terminal_print(int x, int y, const char* s);
+dimensions_t terminal_wprint(int x, int y, const wchar_t* s);
+dimensions_t terminal_printf(int x, int y, const char* s, ...);
+dimensions_t terminal_wprintf(int x, int y, const wchar_t* s, ...);
+dimensions_t terminal_print_ext(int x, int y, int width, int height, int align, const char* s);
+dimensions_t terminal_wprint_ext(int x, int y, int width, int height, int align, const wchar_t* s);
+dimensions_t terminal_printf_ext(int x, int y, int width, int height, int align, const char* s, ...);
+dimensions_t terminal_wprintf_ext(int x, int y, int width, int height, int align, const wchar_t* s, ...);
 
+dimensions_t terminal_measure(const char* s);
+dimensions_t terminal_wmeasure(const wchar_t* s);
+dimensions_t terminal_measuref(const char* s, ...);
+dimensions_t terminal_wmeasuref(const wchar_t* s, ...);
+
+dimensions_t terminal_measure_ext(int width, int height, const char* s);
+dimensions_t terminal_wmeasure_ext(int width, int height, const wchar_t* s);
 
 #ifdef __cplusplus
 }
